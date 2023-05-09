1d87bd3e7482cf04a2eaeb32df460ab74639046d
SlimskiTheWise <hun245600@gmail.com>
2023-05-09 11:55:53 +0900

    test: test for validating user

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -15,6 +15,7 @@ describe('AuthService', () => {
 
   let authService: AuthService;
   let utilsService: UtilsService;
+  let usersService: UsersService;
   let jwtService: JwtService;
   let usersRepository: Repository<Users>;
 
@@ -68,9 +69,52 @@ describe('AuthService', () => {
     utilsService = module.get<UtilsService>(UtilsService);
     jwtService = module.get<JwtService>(JwtService);
     usersRepository = module.get<Repository<Users>>(USER_REPO_TOKEN);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(authService).toBeDefined();
+  });
+
+  describe('validate user', () => {
+    it('should succeed validating user', async () => {
+      const email = 'test@test.com';
+      const password = '123456';
+
+      const user = new Users();
+
+      jest.spyOn(usersService, 'findOne').mockResolvedValue(user);
+      jest.spyOn(utilsService, 'compare').mockResolvedValue(true);
+      const result = await authService.validateUser(email, password);
+
+      expect(result).toBe(user);
+    });
+
+    it('should fail validating user when user does not exist', async () => {
+      const email = 'test@test.com';
+      const password = '123456';
+
+      jest.spyOn(usersService, 'findOne').mockResolvedValue(undefined);
+
+      expect(authService.validateUser(email, password)).rejects.toThrowError();
+    });
+
+    it('should fail validating when password is not matching', async () => {
+      const email = 'test@test.com';
+      const password = '123456';
+
+      const user = new Users();
+      user.email = email;
+      user.password = '121212';
+
+      jest.spyOn(usersService, 'findOne').mockResolvedValue(user);
+      jest.spyOn(utilsService, 'compare').mockResolvedValue(false);
+
+      expect(authService.validateUser(email, password)).rejects.toThrowError();
+    });
   });
 });
