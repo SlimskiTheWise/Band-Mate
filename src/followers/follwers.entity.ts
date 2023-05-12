d22e1e625942efd84dcda54207f56c89c1aeb441
SlimskiTheWise <seunghoon@ziptoss.com>
2023-05-12 16:35:01 +0900

    fix: typo

--- src/followers/follwers.entity.ts
+++ src/followers/follwers.entity.ts
@@ -4,11 +4,11 @@ import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity('Followers')
 export class Followers extends AbstractEntity {
-  @Column({ name: 'follwingUserId', type: 'int', nullable: false })
+  @Column({ name: 'followingUserId', type: 'int', nullable: false })
   followingUserId: number;
 
-  @Column({ name: 'follwedUserId', type: 'int', nullable: false })
-  follwedUserId: number;
+  @Column({ name: 'followedUserId', type: 'int', nullable: false })
+  followedUserId: number;
 
   @OneToMany(() => Users, (user) => user.followers)
   follower: Users;
