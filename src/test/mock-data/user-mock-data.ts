655c57cf25ded28bbea137ad99b0ea1ae6698027
SlimskiTheWise <hun245600@gmail.com>
2023-05-23 15:53:25 +0900

    feat: implement pagination

--- src/test/mock-data/user-mock-data.ts
+++ src/test/mock-data/user-mock-data.ts
@@ -1,15 +1,19 @@
 import { Role } from 'src/common/enums/role.enum';
 import { Users } from 'src/users/users.entity';
 
-export function seedSingleUser(): Users {
+export function seedSingleUser(index = 1): Users {
   const user = new Users();
-  user.id = 1;
-  user.name = 'random-name';
-  user.email = 'test@test.com';
+  user.id = index;
+  user.name = `random-name${index}`;
+  user.email = `test@test${index}.com`;
   user.password = '123456';
   user.createdAt = new Date();
   user.updatedAt = new Date();
   user.deletedAt = null;
   user.role = Role.USER;
   return user;
+}
+
+export function seedManyUsers(): Users[] {
+  return Array.from({ length: 50 }, (_, i) => seedSingleUser(i));
 }
