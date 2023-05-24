c9ad35dc4caa81edb49792ea0f611598f34714d7
SlimskiTheWise <hun245600@gmail.com>
2023-05-24 16:58:59 +0900

    feat: implement verifying a use when signing up

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,22 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { VerificationCodesRepository } from './verification-codes.repository';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly verificationCodesRepository: VerificationCodesRepository,
+  ) {}
 
   async sendVerificationCode(email: string) {
     try {
-      const verificationCode: number = Math.round(Math.random() * 10000);
+      // delete existing verification code data when the user requesting this agian
+      const codeExists = await this.verificationCodesRepository.findOneByEmail(
+        email,
+      );
+      if (codeExists) {
+        this.verificationCodesRepository.deleteVerificationCode(codeExists.id);
+      }
+
+      const code: number = Math.round(Math.random() * 10000);
       await this.mailerService.sendMail({
         to: email,
         from: 'seunghoon@ziptoss.com',
         subject: 'Verification Code',
-        html: `<p>Code: ${verificationCode}</p>`,
+        html: `<p>Code: ${code}</p>`,
       });
-      return verificationCode;
+      await this.verificationCodesRepository.createVerificationCode(
+        code,
+        email,
+      );
     } catch (err) {
       console.log(err);
     }
+  }
+
+  async verifyVerificationCode(code: number, email: string): Promise<boolean> {
+    const isVerified =
+      await this.verificationCodesRepository.verifyVerificationCode(
+        code,
+        email,
+      );
+
+    if (!isVerified) throw new BadRequestException('wrong verification code');
+
+    await this.verificationCodesRepository.updateVerificationCode(
+      isVerified.id,
+    );
+
+    return true;
   }
 }
