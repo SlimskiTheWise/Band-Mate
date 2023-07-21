64e7e5d0186136fa18fc46f4def1353d1fe3aa5b
SlimskiTheWise <hun245600@gmail.com>
2023-07-21 18:23:10 +0900

    feat: add picture column to instruments

--- src/instruments/instruments.controller.ts
+++ src/instruments/instruments.controller.ts
@@ -9,28 +9,45 @@ import {
   ParseIntPipe,
   Param,
   Delete,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { InstrumentsService as InstrumentsService } from './instruments.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Instruments } from './instruments.entity';
 import { CreateInstrumentDto } from './dtos/create-instument.dto';
 import { Users } from 'src/users/users.entity';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { PageDto } from 'src/utils/responses/page.dto';
 import { InstrumentsFindAllDto } from './dtos/instruments-find-all.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AwsService } from 'src/aws/aws.service';
 
 @Controller('instruments')
 @ApiTags('Instruments')
 export class InstrumentsController {
-  constructor(private instrumentsService: InstrumentsService) {}
+  constructor(
+    private instrumentsService: InstrumentsService,
+    private readonly awsService: AwsService,
+  ) {}
 
+  @ApiConsumes('multipart/form-data')
   @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'creating a post for selling' })
   @Post()
+  @UseInterceptors(FileInterceptor('instrumentPicture'))
   async createInstrument(
     @Body() body: CreateInstrumentDto,
     @Req() { user }: { user: Users },
+    @UploadedFile() file: Express.Multer.File,
   ): Promise<Instruments> {
+    if (file) {
+      const { key } = await this.awsService.uploadFileToS3(
+        'test/instrument',
+        file,
+      );
+      body.picture = key;
+    }
     return this.instrumentsService.createInstrument(body, user);
   }
 
--- src/instruments/instruments.module.ts
+++ src/instruments/instruments.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Instruments } from './instruments.entity';
 import { UtilsModule } from 'src/utils/utils.module';
 import { InstrumentsRepository } from './instruments.repository';
+import { AwsModule } from 'src/aws/aws.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Instruments]), UtilsModule],
+  imports: [TypeOrmModule.forFeature([Instruments]), UtilsModule, AwsModule],
   controllers: [InstrumentsController],
   providers: [InstrumentsService, InstrumentsRepository],
 })
